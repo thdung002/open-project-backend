2a2f259e8b02b8556fc413a7714027381b0ffb12
Dung Tran <dung.tran@alphanetworks.global>
2025-04-15 09:02:40 +0700

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,6 @@
 # Add hosts entry
 echo "172.16.0.11 proj.mecury.com.vn" >> /etc/hosts
 
-# Install dependencies
-echo "Installing dependencies..."
-npm install
 
 # Start the application
 exec nodemon server.js 
